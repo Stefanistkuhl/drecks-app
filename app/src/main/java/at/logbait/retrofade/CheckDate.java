2edbe5ebf7fdd6cfbfa9f1ab9c13c3fcd92280a1
Stefanistkuhl <ghgstefan@gmail.com>
2023-02-20 19:36:33 +0100

    some pain ass changes

---
+++ app/src/main/java/at/logbait/retrofade/CheckDate.java
@@ -0,0 +1,23 @@
+package at.logbait.retrofade;
+
+import android.content.Context;
+import android.os.Environment;
+import java.io.File;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+
+public class CheckDate {
+
+    public static boolean checkIfPictureExists(Context context, String filename_exists) {
+
+        SimpleDateFormat datum = new SimpleDateFormat("dd-MM-yyyy");
+        String fileName = datum + ".png";
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyAppImages");
+        File imageFile = new File(storageDir, filename_exists);
+
+        return Files.exists(imageFile.toPath());
+
+
+    }
+
+}
---
+++ app/src/main/java/at/logbait/retrofade/Settings.java
@@ -0,0 +1,62 @@
+package at.logbait.retrofade;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.muttaapp.R;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+public class Settings extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private String selectedColor;
+    private String selectedtextColor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Spinner spinnerselectcolor=findViewById(R.id.spinner_select_color);
+        ArrayAdapter<CharSequence>adapter= ArrayAdapter.createFromResource(this, R.array.colors, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerselectcolor.setAdapter(adapter);
+        spinnerselectcolor.setOnItemSelectedListener(this);
+
+        Spinner spinnerselecttextcolor=findViewById(R.id.spinner_select_text_color);
+        ArrayAdapter<CharSequence>adapter2= ArrayAdapter.createFromResource(this, R.array.text_colors, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerselecttextcolor.setAdapter(adapter2);
+        spinnerselecttextcolor.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView <?> parent, View view, int position, long id){
+        int spinnerid = parent.getId();
+        if(spinnerid == R.id.spinner_select_color){
+            selectedColor = parent.getItemAtPosition(position).toString();
+            SharedPreferences sharedPref = getSharedPreferences("user_prefs", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString("selected_color", selectedColor);
+            editor.apply();
+        }else if (spinnerid == R.id.spinner_select_text_color){
+            selectedtextColor = parent.getItemAtPosition(position).toString();
+            SharedPreferences sharedPref2 = getSharedPreferences("user_text_prefs", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor2 = sharedPref2.edit();
+            editor2.putString("select_text_color", selectedtextColor);
+            editor2.apply();
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView <?> parent){
+
+    }
+
+}
